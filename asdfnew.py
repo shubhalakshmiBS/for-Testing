dd5f368f250d2e333e029ab2029a6485abd3df2a
shubhalakshmi B S <shubhalakshmibs@nousinfo.com>
2024-01-24 09:22:35 +0530

    ReqId 789:added new files

---
+++ asdfnew.py
@@ -0,0 +1,21 @@
+my_list = ['Multiple of 6' if i % 6 == 0 
+           else 'Multiple of 2' if i % 2 == 0 
+           else 'Multiple of 3' if i % 3 == 0 
+           else i for i in range(1, 20)] 
+print(my_list)
+
+
+
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+
+
+#printing statement
+def my_function():
+  print("Hello from a function")
+
+my_function()
